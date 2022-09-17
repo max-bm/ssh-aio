cbdd726c2ea5fcca2d6b20f94d0738dfe1bd2b91
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2022-09-17 23:33:51 +0100

    ssh-copy-id and scp functionality added.

--- ssh-aio.sh
+++ ssh-aio.sh
@@ -6,7 +6,7 @@
 # 	- Option to install vim? nvim? --vim --nvim  <- THIS IS A COOL IDEA
 
 ssh-aio () {
-	# ssh-copy-id
-	# scp vi config
+	ssh-copy-id -i ~/.ssh/id_rsa.pub $1
+	scp /etc/vimrc $1:/etc/vimrc
 	ssh $1
 }
