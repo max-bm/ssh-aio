e1d566b8cf34cb5e936c96ff6510047210b73635
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2022-09-17 22:46:55 +0100

    Basic initial plan in new script file.

---
+++ ssh-aio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ideally runs like a Linux command:
+# 	- Taking remote server as argument
+# 	- Location of ssh-key?
+# 	- Option to install vim? nvim? --vim --nvim  <- THIS IS A COOL IDEA
+
+# ssh-copy-id
+# scp vi config
+# ssh
