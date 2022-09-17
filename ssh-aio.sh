c7774a3c5bb9e5221311d88f0a5d8781bc27e2c1
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2022-09-17 23:36:34 +0100

    Comments updated.

--- ssh-aio.sh
+++ ssh-aio.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Ideally runs like a Linux command:
-# 	- Taking remote server as argument
-# 	- Location of ssh-key?
-# 	- Option to install vim? nvim? --vim --nvim  <- THIS IS A COOL IDEA
+# 	- Taking remote server as argument <- DONE
+# 	- Location of ssh-key? OR check if ssh-aio-key exists, if not, create it?
+# 	- Option to install vim? nvim? --vim --nvim <- THIS IS A COOL IDEA
 
 ssh-aio () {
 	ssh-copy-id -i ~/.ssh/id_rsa.pub $1
