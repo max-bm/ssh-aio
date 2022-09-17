d7c1ca8f84e162028108bd59e45a933e1a972539
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2022-09-18 00:58:14 +0100

    Config file choice added.

--- ssh-aio.sh
+++ ssh-aio.sh
@@ -7,6 +7,20 @@
 
 ssh-aio () {
 	ssh-copy-id -i ~/.ssh/id_rsa.pub $1
-	scp /etc/vimrc $1:/etc/vimrc
+	# Get options
+	while getopts ":p:" option; do
+		case $option in
+			p) # scp vim config
+				#scp /etc/vimrc $1:/etc/vimrc
+				case $OPTARG in
+					vim) # scp vim config
+						scp /etc/vimrc $1:/etc/vimrc	
+						;;
+					# Include other options here!
+				esac
+				echo "${OPTARG}"
+				;;
+		esac
+	done
 	ssh $1
 }
