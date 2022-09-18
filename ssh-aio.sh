4470e11e5be3ec507398df816c19cd3bb161b3bb
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2022-09-18 22:26:19 +0100

    Changed option to -v and removed option argument.

--- ssh-aio.sh
+++ ssh-aio.sh
@@ -9,17 +9,11 @@
 ssh-aio () {
 	ssh-copy-id -i ~/.ssh/id_rsa.pub $1
 	# Get options
-	while getopts ":p:" option; do
+	while getopts ":v" option; do
 		case $option in
-			p) # scp vim config
-				#scp /etc/vimrc $1:/etc/vimrc
-				case $OPTARG in
-					vim) # scp vim config
-						scp /etc/vimrc $1:/etc/vimrc	
-						;;
-					# Include other options here!
-				esac
-				echo "${OPTARG}"
+			v) # scp vim config
+				scp /etc/vimrc $1:/etc/vimrc
+				echo "option v works"
 				;;
 		esac
 	done
